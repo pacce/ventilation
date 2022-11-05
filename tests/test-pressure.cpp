b95130cbc6ef2478efed36aba1264108dd9fe4ac
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2022-11-05 19:48:26 -0300

    feat: tests pressure * implementation

--- tests/test-pressure.cpp
+++ tests/test-pressure.cpp
@@ -36,6 +36,26 @@ RC_GTEST_PROP(Add, Successorf32, (const P32& xs)) {
     RC_ASSERT((xs + ys) != xs);
 }
 
+RC_GTEST_PROP(Mul, Associativef32, (const P32& xs, const P32& ys, const P32& zs)) {
+    RC_ASSERT(((xs * ys) * zs) == (xs * ys * zs));
+}
+
+RC_GTEST_PROP(Mul, Commutativef32, (const P32& xs, const P32& ys)) {
+    RC_ASSERT((xs * ys) == (ys * xs));
+}
+
+RC_GTEST_PROP(Mul, Neutralf32, (const P32& xs)) {
+    P32 ys(1.0f);
+    RC_ASSERT((xs * ys) == xs);
+}
+
+RC_GTEST_PROP(Mul, Zerof32, (const P32& xs)) {
+    P32 ys(0.0f);
+    RC_ASSERT((xs * ys) == ys);
+}
+
+// Tests double precision pressure implementation
+
 RC_GTEST_PROP(Add, Associativef64, (const P64& xs, const P64& ys, const P64& zs)) {
     RC_ASSERT(((xs + ys) + zs) == (xs + ys + zs));
 }
@@ -52,6 +72,24 @@ RC_GTEST_PROP(Add, Neutralf64, (const P64& xs)) {
 RC_GTEST_PROP(Add, Successorf64, (const P64& xs)) {
     P64 ys(1.0);
     RC_ASSERT((xs + ys) != xs);
+}
+
+RC_GTEST_PROP(Mul, Associativef64, (const P64& xs, const P64& ys, const P64& zs)) {
+    RC_ASSERT(((xs * ys) * zs) == (xs * ys * zs));
+}
+
+RC_GTEST_PROP(Mul, Commutativef64, (const P64& xs, const P64& ys)) {
+    RC_ASSERT((xs * ys) == (ys * xs));
+}
+
+RC_GTEST_PROP(Mul, Neutralf64, (const P64& xs)) {
+    P64 ys(1.0f);
+    RC_ASSERT((xs * ys) == xs);
+}
+
+RC_GTEST_PROP(Mul, Zerof64, (const P64& xs)) {
+    P64 ys(0.0f);
+    RC_ASSERT((xs * ys) == ys);
 }
 
 int
