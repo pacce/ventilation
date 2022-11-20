eb58766beb2614a7d30d3afc014cdb6ba049a305
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2022-11-20 19:41:16 -0300

    feat: impls a variant to hold ventilation modes

---
+++ include/ventilation/ventilation-modes/ventilation-modes-empty.hpp
@@ -0,0 +1,11 @@
+#ifndef VENTILATION_MODES_EMPTY_HPP__
+#define VENTILATION_MODES_EMPTY_HPP__
+
+namespace ventilation {
+namespace modes {
+    template <typename Precision>
+    class Empty {};
+} // namespace modes
+} // namespace ventilation
+
+#endif // VENTILATION_MODES_EMPTY_HPP__
--- include/ventilation/ventilation-modes/ventilation-modes.hpp
+++ include/ventilation/ventilation-modes/ventilation-modes.hpp
@@ -1,7 +1,21 @@
 #ifndef VENTILATION_MODES_HPP__
 #define VENTILATION_MODES_HPP__
 
+#include "ventilation-modes-empty.hpp"
 #include "ventilation-modes-pcv.hpp"
 #include "ventilation-modes-vcv.hpp"
+#include <variant>
+
+namespace ventilation {
+    template <typename Precision>
+    using Modes = std::variant<
+          modes::Empty<Precision>
+        , modes::PCV<Precision>
+        , modes::VCV<Precision>
+        >;
+namespace modes {
+    enum class Names { EMPTY, PCV, VCV };
+} // namespace modes
+} // namespace ventilation
 
 #endif // VENTILATION_MODES_HPP__
